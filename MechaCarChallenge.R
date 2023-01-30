f3496cb1d999dc8b368f5303ed1e7d98e402b8c4
albertoaignr <albertoaigner@gmail.com>
2023-01-30 10:35:18 -0500

    Updated, deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -19,3 +19,40 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle +
 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle +
      ground_clearance + AWD, data=MechaCar_mpg))
+
+# Deliverable 2
+
+
+# Import and read csv file as a table  
+
+SuspensionCoil <- read.csv('Resources/Suspension_Coil.csv')
+
+# Script that creates a total summary dataframe of psi column 
+
+total_summary <- SuspensionCoil %>% summarize(Mean = mean(PSI), 
+                           Median = median(PSI), 
+                           Variance = var(PSI),
+                           SD = sd(PSI))
+
+
+# Script that creates a lot summary grouping by manufacturing lot 
+
+lot_summary <- SuspensionCoil %>% group_by(Manufacturing_Lot) %>%
+  summarize(Mean = mean(PSI), 
+            Median = median(PSI), 
+            Variance = var(PSI),
+            SD = sd(PSI), .groups= 'keep')
+
+
+  
+  
+  
+  
+  
+  
+  
+
+
+
+
+
